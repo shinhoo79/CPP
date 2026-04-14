c9be4fba5e73de5a76d78ee8b83694e1fc7efe47
shinhoo79 <hooshin79@naver.com>
2026-04-14 11:48:39 +0900

    first commit

---
+++ Week7/p301_1/main.cpp
@@ -0,0 +1,23 @@
+#include "MyTime.h"
+
+int main() {
+    MyTime t1, t2, t3;
+
+    t1.read();
+    t2.read();
+
+    t3 = t1.add(t2);
+
+    t1.print();
+    t2.print();
+    t3.print();
+
+    t3.reset();
+    t3.print();
+
+    MyTime t4;
+    t4.convert(3723.567);
+    t4.print();
+
+    return 0;
+}
